d7df822aa95e4bc3ce30583f2f098c0b68517eb1
Johnson Mohamed <mohamedjohnson31@gmail.com>
2021-02-05 10:10:26 +0000

    :bug: Image Ratio Corrected

--- next-seo.config.js
+++ next-seo.config.js
@@ -2,14 +2,14 @@ export default {
   title: 'Acceuil',
   titleTemplate: '%s | JudiMap Orientation',
   description: 'JUDIMAP est la première plateforme digitale dédiée à l’information juridique et administrative au Sénégal.',
-  canonical: 'https://judimap.netlify.app',
+  canonical: 'https://judimap.vercel.app',
   openGraph: {
     type: 'website',
     locale: 'fr_FR',
-    url: 'https://judimap.netlify.app/',
+    url: 'https://judimap.vercel.app',
     site_name: 'JudiMap Orientation',
     images: [
-      { url: './public/logo.jpeg' }
+      { url: 'public/logo.jpeg' }
     ]
   },
   twitter: {
